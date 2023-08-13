8c6e88f210dcfe97446ea2567973d86da71b4834
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-08-13 03:35:46 +0100

    ajouter de sendpoint

--- income_expense/authentication/urls.py
+++ income_expense/authentication/urls.py
@@ -1,6 +1,8 @@
-from .views import RegistrationView
+from .views import RegistrationView,UsernameValidation
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('registre',RegistrationView.as_view(),name='registre')
+    path('registre',RegistrationView.as_view(),name='registre'),
+    path('valide-username',csrf_exempt(UsernameValidation.as_view()),name='validation username')
 ]
--- income_expense/authentication/views.py
+++ income_expense/authentication/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
+
 
 # Create your views here.
+class UsernameValidation(View):
+    def post(self,request):
+        data=json.loads(request.body)
+        username=data['username']
+        
+        if not str(username).isalnum():
+            return JsonResponse({'username_error':'username only with alphanumerique char'},status=400)
+        
+        return JsonResponse({'username-valid':username})
+    
 class RegistrationView(View):
     def get(self,request):
         return render(request,'authentication/registre.html')
