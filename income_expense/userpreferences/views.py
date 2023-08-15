6b4a47c96475e15fdd80481dcf2249651329cb0e
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-08-15 21:47:30 +0100

    add currency

--- income_expense/userpreferences/views.py
+++ income_expense/userpreferences/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+import os
+import json
+from django.conf import settings
 # Create your views here.
 def index(request):
+    currency_data=[]
+    file_path=os.path.join(settings.BASE_DIR,'currencies.json')
+    
+    with open(file_path,"r") as json_file:
+        data=json.load(json_file)
+        for k,v in data.items():
+            currency_data.append({'name':k,'value':v})
+        
+        
+    import pdb
+    pdb.set_trace()
+    
     return render(request,'preferences/index.html')
