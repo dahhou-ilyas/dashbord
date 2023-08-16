7ffb60d29afac0b76e4d4bb24aeaff2ef36d33db
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-08-16 18:06:36 +0100

    CREATE, READ, UPDATE AND DELETE EXPENSES

--- income_expense/expenses/urls.py
+++ income_expense/expenses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
    path('',views.index,name='expenses'),
+   path('add-expense', views.add_expences, name="add-expenses"),
 ]
--- income_expense/expenses/views.py
+++ income_expense/expenses/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from .models import Category,Expense
+from django.contrib import messages
 # Create your views here.
 
 @login_required(login_url="/authentication/login")
 @never_cache
 def index(request):
+    category=Category.objects.all()
     return render(request,'expenses/index.html')
+
+def add_expences(request):
+    category=Category.objects.all()
+    context={
+        'categories':category,
+        'values':request.POST
+    }
+    print(request.POST)
+
+    if request.method=='POST':
+        amount=request.POST['amount']
+        description=request.POST['description']
+        if not amount:
+            messages.error(request,'Ammount is required')
+            return render(request,'expenses/add_expense.html',context)
+        if not description:
+            messages.error(request,'description is required')
+            return render(request,'expenses/add_expense.html',context)
+    return render(request,'expenses/add_expense.html',context)
+        
+    
