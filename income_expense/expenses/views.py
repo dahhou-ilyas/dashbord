0a372662a27f7ce66fa43111da4bc7663369a44b
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-08-16 19:02:45 +0100

    add table

--- income_expense/expenses/views.py
+++ income_expense/expenses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from .models import Category,Expense
@@ -8,8 +8,13 @@ from django.contrib import messages
 @login_required(login_url="/authentication/login")
 @never_cache
 def index(request):
+    
     category=Category.objects.all()
-    return render(request,'expenses/index.html')
+    expenses=Expense.objects.filter(owner=request.user)
+    context={
+        'expenses':expenses
+    }
+    return render(request,'expenses/index.html',context)
 
 def add_expences(request):
     category=Category.objects.all()
@@ -17,17 +22,23 @@ def add_expences(request):
         'categories':category,
         'values':request.POST
     }
-    print(request.POST)
+
 
     if request.method=='POST':
         amount=request.POST['amount']
         description=request.POST['description']
+        expense_date=request.POST['expense_date']
+        categori=request.POST['category']
         if not amount:
             messages.error(request,'Ammount is required')
             return render(request,'expenses/add_expense.html',context)
         if not description:
             messages.error(request,'description is required')
             return render(request,'expenses/add_expense.html',context)
+        Expense.objects.create(owner=request.user,amount=amount,date=expense_date,
+                               category=categori,description=description)
+        messages.success(request,'Expense saved succesffuly')
+        return redirect('expenses')
     return render(request,'expenses/add_expense.html',context)
         
     
